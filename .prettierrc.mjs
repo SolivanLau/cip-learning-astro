9e7103fb99abddb33417e28b289c10a3150f6f0d
solivan-lau <solivan_lau@outlook.com>
2024-12-23 16:38:18 -0500

    COMPONENTS & LAYOUT: Finished button component, created MVP for card and list components

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,17 +1,17 @@
 /** @type {import("prettier").Config} */
 export default {
-	semi: true,
-	singleQuote: false,
-	tabWidth: 4,
-	useTabs: true,
-	trailingComma: "es5",
-	plugins: ["prettier-plugin-astro"],
-	overrides: [
-		{
-			files: "*.astro",
-			options: {
-				parser: "astro",
-			},
-		},
-	],
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  semi: true,
+  singleQuote: false,
+  tabWidth: 4,
+  useTabs: true,
+  trailingComma: "es5",
 };
